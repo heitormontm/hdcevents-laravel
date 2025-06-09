491f3e15e01b3e1762c5b1152a701b8f8d133cad
tortwz <heitormacedo1234@gmail.com>
2025-06-09 15:17:53 -0300

    Saving JSON to the database

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $casts = [
+        'items' => 'array'
+    ];
+    
 }
